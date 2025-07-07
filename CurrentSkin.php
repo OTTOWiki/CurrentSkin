2581efce36e4b267f210c2cbc32f494098f384d9
电棍老板 <89052130+12306ylg@users.noreply.github.com>
2025-07-07 09:55:19 +0800

    Update CurrentSkin.php

--- CurrentSkin.php
+++ CurrentSkin.php
@@ -4,7 +4,7 @@ class CurrentSkin {
 	/**
 	 * Register magic word ID
 	 */
-	public static function onMagicWordwgVariableIDs( &$variableIds ) {
+	public static function onMagicWordwgVariableIDs( array &$variableIds ): bool {
 		$variableIds[] = 'CURRENTSKIN';
 		return true;
 	}
@@ -13,23 +13,28 @@ class CurrentSkin {
 	 * Magic word processing logic
 	 */
 	public static function onParserGetVariableValueSwitch(
-		$parser,
-		&$cache,
-		$magicWordId,
+		Parser $parser,
+		array &$cache,
+		string $magicWordId,
 		&$ret,
-		$frame
-	) {
-		if ( $magicWordId === 'CURRENTSKIN' ) {
-			$services = \MediaWiki\MediaWikiServices::getInstance();
-			$userOptionsLookup = $services->getUserOptionsLookup();
-			$user = $parser->getUserIdentity();
-			$skin = $userOptionsLookup->getOption( $user, 'skin' );
-			
-			// Disable page caching (user-specific data)
-			$parser->getOutput()->updateCacheExpiry(0);
-			
-			$ret = $skin;
+		PPFrame $frame
+	): bool {
+		// Early return if not our magic word
+		if ( $magicWordId !== 'CURRENTSKIN' ) {
+			return true;
 		}
+
+		$services = \MediaWiki\MediaWikiServices::getInstance();
+		$userOptionsLookup = $services->getUserOptionsLookup();
+		$user = $parser->getUserIdentity();
+		
+		// Fetch user's skin preference
+		$skin = $userOptionsLookup->getOption( $user, 'skin' );
+		
+		// Disable page caching (user-specific data)
+		$parser->getOutput()->updateCacheExpiry(0);
+		
+		$ret = $skin;
 		return true;
 	}
 }
