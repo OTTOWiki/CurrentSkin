da72555eaa94d0e65d7bf5e849549296f0377470
电棍老板 <89052130+12306ylg@users.noreply.github.com>
2025-07-07 13:07:56 +0800

    Update CurrentSkin.i18n.magic.php

--- CurrentSkin.i18n.magic.php
+++ CurrentSkin.i18n.magic.php
@@ -1,6 +1,16 @@
 <?php
+
 $magicWords = [];
 
 $magicWords['en'] = [
-	'CURRENTSKIN' => [0, 'CURRENTSKIN']
+	'CURRENTSKIN' => [ 0, 'CURRENTSKIN' ],
+];
+$magicWords['zh-hans'] = [
+	'CURRENTSKIN' => [ 0, '当前皮肤' ],
+];
+$magicWords['zh-hant'] = [
+	'CURRENTSKIN' => [ 0, '當前外觀' ],
+];
+$magicWords['de'] = [
+    'CURRENTSKIN' => [ 0, 'AKTUELLESSKIN' ],
 ];
